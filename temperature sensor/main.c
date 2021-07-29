781e561cdee67523551122ade608a87a268e2e8d
Makkapati Ganesh <83875009+MakkapatiGanesh@users.noreply.github.com>
2021-07-29 16:54:16 +0530

    Add files via upload
    See for [filename.c] .c files for codes

---
+++ temperature sensor/main.c
@@ -0,0 +1,33 @@
+#include <msp430.h>
+#include<math.h>
+volatile float results;
+volatile float temparature;
+int main(void)
+{
+ volatile unsigned int i;
+  WDTCTL = WDTPW+WDTHOLD;                   // Stop watchdog timer
+  P6SEL |= BIT0;                            // Enable A/D channel A0
+  REFCTL0 &= ~REFMSTR;                      // Reset REFMSTR to hand over control to
+                                            // ADC12_A ref control registers
+  ADC12CTL0 = ADC12ON+ADC12SHT02+ADC12REFON+ADC12REF2_5V;//using internal reference module with 2.5 as vref
+                                            // Turn on ADC12, Sampling time in 4x multiples as 02 16x multiples of adcclk.
+                                            // On Reference Generator and set to
+                                            // 2.5V
+  ADC12CTL1 = ADC12SHP;                     // Use sampling timer
+  ADC12MCTL0 = ADC12SREF1;                 // Vr+=Vref+ and Vr-=AVss
+
+  for ( i=0; i<0x30; i++);                  // Delay for reference start-up
+
+  ADC12CTL0 |= ADC12ENC;                    // Enable conversions
+  volatile int k;
+  while (1)
+  {
+
+    ADC12CTL0 |= ADC12SC;
+    k=ADC12MEM0;// Start conversion
+   results = (ADC12MEM0 *0.00061)-0.0246;
+   temparature=results*100;
+
+
+  }
+}
